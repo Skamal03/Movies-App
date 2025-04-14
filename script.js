85a82c707ed30a3525b3d44d078f8ccdd380bb20
Sunnan Kamal <79265528+Skamal03@users.noreply.github.com>
2025-04-15 04:02:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = 'heheheh';
-const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+const apiUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`;
 
 const movieContainer = document.querySelector('.movies');
 
